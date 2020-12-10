3f4a20aaec5f6bf32461318550559a13ba39852e
Moritz-Hoeppner <moritz.hoeppner@resourcify.de>
2020-12-10 16:57:59 +0000

    Improved code

--- Main.java
+++ Main.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Whats your name?");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        // Hier ein Hallo <Name> Statement
     }
-    
+
 }
+
+
+/*
+* Tip:
+* System.out.println("Hello " + name);
+*/
